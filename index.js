9b2470e9e355ff32b718e2693778eb3c3817ef46
Matt Murtagh <mattm@marketamerica.com>
2020-03-26 13:58:59 -0400

    remove logging

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const { Request } = require('./lib/request')
 module.exports = {
   plugin: loggerPlugin,
   logServiceCall: (serviceCall = {}, requestId) => {
-    const { url = ''  } = serviceCall 
-
-    if (!requestId) {
-      console.warn(`graphql-service-logger received empty requestId for the following service call: ${url}`)
-      return
-    }
-
     Request.associateServiceCall(requestId, serviceCall)
   }
 }
